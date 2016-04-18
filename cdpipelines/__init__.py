5426c0d004dbbfcf12068a9f5faae748a97f5043
Christopher DeBoever <cdeboever3@gmail.com>
2016-04-18 10:56:29 -0700

    Updates for non-python command line scripts
    I think I have this working now but I need to test. I've updated the
    version number so I can push a new version to pypi.

--- cdpipelines/__init__.py
+++ cdpipelines/__init__.py
@@ -5,7 +5,8 @@ import convert_bed_to_saf
 
 _root = os.path.split(os.path.split(os.path.abspath(__file__))[0])[0]
 
-_scripts = os.path.join(os.path.abspath(__file__), 'scripts')
+_scripts = os.path.join(os.path.split(os.path.abspath(__file__))[0], 'scripts')
+# _scripts = os.path.join(os.path.abspath(__file__), 'scripts')
 # _scripts = os.path.join(
 #     os.path.sep.join(os.path.abspath(__file__).split(os.path.sep)[0:-2]),
 #     'scripts')
